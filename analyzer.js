93d20d22675686e47b5194bb3bab18ec522b7b51
codycaldwell-twc <cody.caldwell@weather.com>
2018-01-14 22:22:15 -0500

    fix undefined issue with combined profile doc analysis and clean up

--- analyzer.js
+++ analyzer.js
@@ -1,262 +1,195 @@
 var cfenv = require("cfenv");
+var reduce = require('lodash/reduce');
 var config = require('./config');
 
 var tone_analyzer;
 
 if (config.services['tone_analyzer']) {
-	console.log('found service tone_analyzer');
-	var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-	var credentials = config.services['tone_analyzer'][0].credentials;
-	credentials['version_date'] = '2016-05-19';
-	tone_analyzer = new ToneAnalyzerV3(credentials)
+  console.log('found service tone_analyzer');
+  var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+  var credentials = config.services['tone_analyzer'][0].credentials;
+  credentials['version_date'] = '2016-05-19';
+  tone_analyzer = new ToneAnalyzerV3(credentials)
 }
 
 function sortByAbsoluteValueDescending(a, b) {
-    if (Math.abs(a.difference) < Math.abs(b.difference)) {
-        return 1;
-    }
-    if (Math.abs(a.difference) > Math.abs(b.difference)) {
-        return -1;
-    }
-    return 0;
+  if (Math.abs(a.difference) < Math.abs(b.difference)) {
+    return 1;
+  }
+  if (Math.abs(a.difference) > Math.abs(b.difference)) {
+    return -1;
+  }
+  return 0;
 }
 
 function compareDocumentLevel(profile_analysis, new_analysis, tone_categories) {
-    var raw_comparison = new_analysis.document_tone.tone_categories; //this sets the base structure for our raw response
-    var ordered_comparison = [];
-    var difference_sum = 0;
-    //Iterate over the categories (emotion, language, social)
-    for (c in raw_comparison) {
-        var new_category = raw_comparison[c];
-        //check if the category should be ignored
-        //this currently results in null values in the returned array rather than 
-        //  changing the size of the array. could be fixed by array.splice and iterating backwards
-        if (!tone_categories.includes(new_category.category_id)) {
-            delete raw_comparison[c];
-            break;
-        }
-        var profile_category = profile_analysis.document_tone.tone_categories[c];
-        // Iterate over tones of each category
-        for (t in new_category.tones) {
-            var new_tone = new_category.tones[t];
-            var profile_tone = profile_category.tones[t];
-            new_tone.difference = profile_tone.score - new_tone.score;
-            difference_sum += Math.abs(new_tone.difference);
-            delete new_tone.score; //removing unnecessary score property
-            var obj = {
-                "category_id" : new_category.category_id,
-                "tone_id": new_tone.tone_id,
-                "difference": new_tone.difference
-            };
-            ordered_comparison.push(obj);
-        }
+  var raw_comparison = new_analysis.document_tone.tone_categories; //this sets the base structure for our raw response
+  var ordered_comparison = [];
+  var difference_sum = 0;
+  //Iterate over the categories (emotion, language, social)
+  for (c in raw_comparison) {
+    var new_category = raw_comparison[c];
+    //check if the category should be ignored
+    //this currently results in null values in the returned array rather than
+    //  changing the size of the array. could be fixed by array.splice and iterating backwards
+    if (!tone_categories.includes(new_category.category_id)) {
+      delete raw_comparison[c];
+      break;
     }
-    ordered_comparison.sort(sortByAbsoluteValueDescending);
+    var profile_category = profile_analysis.document_tone.tone_categories[c];
+    // Iterate over tones of each category
+    for (t in new_category.tones) {
+      var new_tone = new_category.tones[t];
+      var profile_tone = profile_category.tones[t];
+      new_tone.difference = profile_tone.score - new_tone.score;
+      difference_sum += Math.abs(new_tone.difference);
+      delete new_tone.score; //removing unnecessary score property
+      var obj = {
+        "category_id" : new_category.category_id,
+        "tone_id": new_tone.tone_id,
+        "difference": new_tone.difference
+      };
+      ordered_comparison.push(obj);
+    }
+  }
+  ordered_comparison.sort(sortByAbsoluteValueDescending);
 
-    var summary = {};
-    summary.difference_sum = difference_sum;
-    summary.ordered_comparison = ordered_comparison;
-    summary.raw_comparison = raw_comparison;
-    return summary;
+  var summary = {};
+  summary.difference_sum = difference_sum;
+  summary.ordered_comparison = ordered_comparison;
+  summary.raw_comparison = raw_comparison;
+  return summary;
 }
 
 function compareSentenceLevel(profile_analysis, new_analysis, tone_categories) {
-    if (!new_analysis.sentences_tone) {
-        return {
-            "error" : "New content does not contain multiple sentences. Sentence level comparison cannot be made."
-        }
+  if (!new_analysis.sentences_tone) {
+    return {
+        "error" : "New content does not contain multiple sentences. Sentence level comparison cannot be made."
     }
-    var raw_comparison = new_analysis.sentences_tone; //sets the base structure for our response
-    //Iterate over the new content sentences analysis
-    for (s in raw_comparison) {
-        var new_sentence = raw_comparison[s];
-        new_sentence.difference_sum = 0;
-        //Iterate over the categories (emotion, language, social)
-        for (c in new_sentence.tone_categories) {
-            var new_sentence_category = new_sentence.tone_categories[c];
-            var profile_category = profile_analysis.document_tone.tone_categories[c];
-            // Iterate over tones of each category
-            for (t in new_sentence_category.tones) {
-                var new_sentence_tone = new_sentence_category.tones[t];
-                var profile_tone = profile_category.tones[t];
-                new_sentence_tone.difference = profile_tone.score - new_sentence_tone.score;
-                new_sentence.difference_sum += Math.abs(new_sentence_tone.difference);
-                delete new_sentence_tone.score; //removing unnecessary score property
-            }
-        }
+  }
+  var raw_comparison = new_analysis.sentences_tone; //sets the base structure for our response
+  //Iterate over the new content sentences analysis
+  for (s in raw_comparison) {
+    var new_sentence = raw_comparison[s];
+    new_sentence.difference_sum = 0;
+    //Iterate over the categories (emotion, language, social)
+    for (c in new_sentence.tone_categories) {
+      var new_sentence_category = new_sentence.tone_categories[c];
+      var profile_category = profile_analysis.document_tone.tone_categories[c];
+      // Iterate over tones of each category
+      for (t in new_sentence_category.tones) {
+        var new_sentence_tone = new_sentence_category.tones[t];
+        var profile_tone = profile_category.tones[t];
+        new_sentence_tone.difference = profile_tone.score - new_sentence_tone.score;
+        new_sentence.difference_sum += Math.abs(new_sentence_tone.difference);
+        delete new_sentence_tone.score; //removing unnecessary score property
+      }
     }
-
-    var summary = {};
-    summary.raw_comparison = raw_comparison;
-    summary.original = {
-        "new": new_analysis,
-        "profile": profile_analysis
-    };
-    return summary;
+  }
+  var summary = {};
+  summary.raw_comparison = raw_comparison;
+  summary.original = {
+    "new": new_analysis,
+    "profile": profile_analysis
+  };
+  return summary;
 }
 
-function combineResultValues(combined_analysis, new_analysis) { 
-    if(combined_analysis == null || combined_analysis == new_analysis) {
-        return new_analysis;
-    }
-    var doc_doc = combined_analysis.document_tone; 
-    
-    for(i in doc_doc.tone_categories) {
-        var doc_cat = doc_doc.tone_categories[i];
-        for(j in doc_cat.tones) {
-            var doc_tone = doc_cat.tones[j];
+function combineResultValues(combined_analysis, new_analysis) {
+  if (!combined_analysis || combined_analysis === new_analysis) {
+      return new_analysis;
+  }
+  var doc_doc = combined_analysis.document_tone;
 
-            var new_doc = new_analysis.document_tone; 
-            for(k in new_doc.tone_categories) {
-                var new_cat = new_doc.tone_categories[k];
-                for(l in new_cat.tones) { 
-                    var new_tone = new_cat.tones[l];
-                    if(doc_tone.tone_id == new_tone.tone_id) {
-                        doc_tone.score += new_tone.score
-                    }
-                }
-            }
+  for (i in doc_doc.tone_categories) {
+    var doc_cat = doc_doc.tone_categories[i];
+    for (j in doc_cat.tones) {
+      var doc_tone = doc_cat.tones[j];
+      var new_doc = new_analysis.document_tone;
+      for (k in new_doc.tone_categories) {
+        var new_cat = new_doc.tone_categories[k];
+        for (l in new_cat.tones) {
+          var new_tone = new_cat.tones[l];
+          if (doc_tone.tone_id == new_tone.tone_id) {
+            doc_tone.score += new_tone.score
+          }
         }
+      }
     }
-    
-    var new_sec = new_analysis.sentences_tone;
-    if(combined_analysis.sentences_tone == null) {
-        combined_analysis.sentences_tone = {};
-    }
-    for(i in new_sec) {
-        var new_tone = new_sec[i];
-        combined_analysis.sentences_tone.push(new_tone)
-    }
-    return combined_analysis;
+  }
+
+  var new_sec = new_analysis.sentences_tone;
+  if (combined_analysis.sentences_tone == null) {
+    combined_analysis.sentences_tone = {};
+  }
+  for (i in new_sec) {
+    var new_tone = new_sec[i];
+    combined_analysis.sentences_tone.push(new_tone)
+  }
+  return combined_analysis;
 }
 
 function averageScores(combined_analysis, count) {
-    var doc_doc = combined_analysis.document_tone; 
+  var doc_doc = combined_analysis.document_tone;
 
-    for(i in doc_doc.tone_categories) {
-        var doc_cat = doc_doc.tone_categories[i];
-        for(j in doc_cat.tones) {
-            var doc_tone = doc_cat.tones[j];
+  for (i in doc_doc.tone_categories) {
+    var doc_cat = doc_doc.tone_categories[i];
+    for (j in doc_cat.tones) {
+      var doc_tone = doc_cat.tones[j];
 
-            doc_tone.score = doc_tone.score / count;
-        }
+      doc_tone.score = doc_tone.score / count;
     }
-    return  combined_analysis
+  }
+  return  combined_analysis
 }
 
-function toneAnalyzeNotCombinedProfile(request_body) {
-    var profile_params = {};
-    profile_params.text = request_body.profile_docs[0].text;
-    var count = 0;
-    var combinedResponse
-
-    for(i in request_body.profile_docs) {
-        tone_analyzer.tone(profile_params, function(error, res) {
-            if (error) 
-                response.send(error);
-            else {
-                combinedResponse = combineResultValues(combinedResponse, res);
-                count += 1;
-                if(count == request_body.profile_docs.length) {
-                    return combinedResponse
-                }
-            }
-        });
-    }
+// Wrap the tone analysis API call in a Promise so that
+// we can take advantage of Promise.all for parallel execution
+function analyzeTone(params) {
+  return new Promise(function(resolve, reject) {
+    tone_analyzer.tone(params, function (error, res) {
+      return error ? reject(error) : resolve(res);
+    });
+  });
 }
 
 module.exports = {
-	analyze: function (request_body, response) {
-        var new_params = {};
-        new_params.text = request_body.new_doc.text;
-        var profile_params = {};    
+  analyze: function (request_body, response) {
+    if (!request_body.combine_profile_docs) {
+      var profile_doc_promises = request_body.profile_docs.map(doc => analyzeTone(doc));
+      var new_doc_promise = analyzeTone(request_body.new_doc);
 
-        if (!request_body.combine_profile_docs) {
-            profile_params.text = request_body.profile_docs[0].text;
-            var count = 0;
-            var combinedResponse // needs to be null for default value
-            var promises = [];
+      Promise.all([...profile_doc_promises, new_doc_promise]).then(values => {
+        // The new doc values will always be the last result
+        var new_analysis = values.pop();
+        var profile_doc_values = values;
+        var combinedResponse = reduce(profile_doc_values, (result, value) => combineResultValues(result, value));
+        var profile_analysis = averageScores(combinedResponse, request_body.profile_docs.length);
 
-            for(i in request_body.profile_docs) {
-                promises.push(new Promise((resolve, reject) => {
-                    tone_analyzer.tone(profile_params, function(error, res) {
-                        if (error) 
-                            reject(error);
-                        else {
-                            resolve(res);
-                        }
-                    });
-                }))
-            }
+        var comparisonFunc = request_body.comparison_level === 'sentence' ? compareSentenceLevel : compareDocumentLevel;
+        response.send(comparisonFunc(profile_analysis, new_analysis, request_body.tone_categories));
+      }).catch((error) => {
+        response.send(error);
+      });
+    } else {
+      var profile_params = {};
+      var new_params = {};
 
-            Promise.all(promises).then(values => {
-                for(i in values) {
-                    combinedResponse = combineResultValues(combinedResponse, values[i]);
-                }
+      // Combine all profile text separated by spaces
+      profile_params.text = request_body.profile_docs.map(doc => doc.text).join(' ');
+      new_params = request_body.new_doc;
 
-                var avg = averageScores(combinedResponse, request_body.profile_docs.length);
+      var profile_analysis_promise = analyzeTone(profile_params);
+      var new_analysis_promise = analyzeTone(new_params);
+      Promise.all([profile_analysis_promise, new_analysis_promise]).then(function(res) {
+        var profile_analysis = res[0];
+        var new_analysis = res[1];
 
-                tone_analyzer.tone(new_params, function(error, res) {
-                    if (error)  {
-                        response.send(error);
-                    }
-                    else {
-                        // pass in analyzed profile and new_doc text to comparison
-                        if (request_body.comparison_level === 'sentence') {
-                            response.send(compareSentenceLevel(avg, res, request_body.tone_categories));
-                        } else if (request_body.comparison_level === 'document') {
-                            response.send(compareDocumentLevel(avg, res, request_body.tone_categories));
-                        }
-                        else {
-                            response.send({
-                                "error" : "Unsupported compare_level provided"
-                            });
-                        }
-                    }
-                });
-            }).catch((error) => {
-                console.log(error);
-            });
-        } else {
-            var combined_profile_doc = '';
-            for (i in request_body.profile_docs) {
-                combined_profile_doc += request_body.profile_docs[i].text;
-                combined_profile_doc += " ";
-            }
-           
-            profile_params.text = combined_profile_doc;
-
-            var profile_analysis = {};
-            var new_analysis = {};
-            // analyze profile text
-            tone_analyzer.tone(profile_params, function (error, res) {
-                if (error) 
-                    response.send(error);
-                else {
-                    profile_analysis = res;
-                    // analyze new doc text
-                    tone_analyzer.tone(new_params, function(error, res) {
-                        if (error) 
-                            response.send(error);
-                        else {
-                            new_analysis = res;
-                            // pass in analyzed profile and new_doc text to comparison
-                            if (request_body.comparison_level === 'sentence') {
-                                response.send(compareSentenceLevel(profile_analysis, new_analysis, request_body.tone_categories));
-                            } else if (request_body.comparison_level === 'document') {
-                                response.send(compareDocumentLevel(profile_analysis, new_analysis, request_body.tone_categories));
-                            }
-                            else {
-                                response.send({
-                                    "error" : "Unsupported compare_level provided"
-                                });
-                            }
-                        }
-                    });
-                }
-            });
-        }
-
-        
-	}
+        var comparisonFunc = request_body.comparison_level === 'sentence' ? compareSentenceLevel : compareDocumentLevel;
+        response.send(comparisonFunc(profile_analysis, new_analysis, request_body.tone_categories));
+      }).catch(function(error) {
+        response.send(error);
+      });
+    }
+  }
 }
